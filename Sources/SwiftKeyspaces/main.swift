8d47f34540039e36658e364b44a9b73c00c85824
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-12-19 10:14:01 -0500

    Building within container, make sure to use static libs

--- Sources/SwiftKeyspaces/main.swift
+++ Sources/SwiftKeyspaces/main.swift
@@ -1,4 +1,5 @@
 import AWSLambdaRuntime
+import CassandraClient
 
 struct Input: Codable {
 	let name: String
@@ -8,6 +9,30 @@ struct Output: Codable {
 	let message: String
 }
 
+struct TableModel: Codable {
+	let key: String
+	let value: String
+}
+
+var configuration = CassandraClient.Configuration(contactPointsProvider: { callback in
+	let points = ["cassandra.us-east-2.amazonaws.com"]
+
+	callback(.success(points))
+}, port: 9142, protocolVersion: .v4)
+
+configuration.consistency = .localQuorum
+configuration.username = "abc"
+configuration.password = "def"
+configuration.keyspace = "test"
+
+var ssl = CassandraClient.Configuration.SSL()
+
+ssl.verifyFlag = CassandraClient.Configuration.SSL.VerifyFlag.none
+ssl.cert = "path/to/sf-class2-root.crt"
+//configuration.ssl = ssl
+
+let cassandra = CassandraClient(configuration: configuration)
+
 Lambda.run { (context, input: Input, completion: @escaping (Result<Output, Error>) -> Void) in
-	completion(.success(Output(message: "Hello \(input.name)")))
+	completion(.success(Output(message: "world")))
 }
