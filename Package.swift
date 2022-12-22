f839f71a0345c2ff5426946cf85505a3a085758c
Matt <85322+mattmassicotte@users.noreply.github.com>
2022-12-22 09:23:21 -0500

    Use events

--- Package.swift
+++ Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftKeyspaces",
+	platforms: [.macOS(.v12)],
     dependencies: [
 		.package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", .upToNextMajor(from: "0.5.0")),
 		.package(url: "https://github.com/apple/swift-cassandra-client", .upToNextMajor(from: "0.1.0")),
@@ -13,6 +14,7 @@ let package = Package(
             name: "SwiftKeyspaces",
             dependencies: [
 				.product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+				.product(name: "AWSLambdaEvents", package: "swift-aws-lambda-runtime"),
 				.product(name: "CassandraClient", package: "swift-cassandra-client"),
 			]),
         .testTarget(
